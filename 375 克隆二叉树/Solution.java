a654f0d827783d473556e13850c2f05e3412176c
moonleo <yanhan_90@126.com>
2018-02-25 13:01:00 +0800

    Lintcode No.375

---
+++ 375 克隆二叉树/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: The root of binary tree
+     * @return: root of new tree
+     */
+    public TreeNode cloneTree(TreeNode root) {
+        // write your code here
+        if(root == null) {
+            return null;
+        }
+        TreeNode clone = new TreeNode(root.val);
+        clone.left = cloneTree(root.left);
+        clone.right = cloneTree(root.right);
+        return clone;
+    }
+}
